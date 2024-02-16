db4e737db9dcdc57dbcb4955f70d2dd6ce952b3e
benyamindsmith <benjamin@alterclimate.com>
2024-02-15 23:34:16 -0500

    marvel network started

--- R/load_data.R
+++ R/load_data.R
@@ -11,3 +11,13 @@
 # igraph::V(game_of_thrones)$name <- got_nodes$Label
 #
 # save(game_of_thrones, file="./data/game_of_thrones.rda")
+
+# marvel_edges <-  readr::read_csv("./inst/sample-datasets/marvel/marvel-unimodal-edges.csv")
+# marvel_nodes <- readr::read_csv("./inst/sample-datasets/marvel/marvel-unimodal-nodes.csv")
+#
+# marvel_network <- igraph::read_graph("./inst/sample-datasets/marvel/marvel-network.graphml",
+#                                                                  format="graphml")
+#
+# igraph::V(marvel_network)$name <- marvel_nodes$Label
+#
+# save(marvel_network, file="./data/marvel_network.rda")
